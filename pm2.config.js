69f9bf48e609f30012cd37b77b675b5b9990c05a
nick-tharwal <sharmanikhil6922@gmail.com>
2024-04-02 02:37:08 +0530

    removed pm2 config

--- pm2.config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  name: "moderation-bot", // Name of application
-  script: "./index.ts", // Entry point of application
-  interpreter: "~/.bun/bin/bun", // Interpreter for application
-  error_file: "logs/error.log", // Error log file
-  out_file: "logs/out.log", // Output log file
-};
