77db482ac0c923b05dfa863edc44c9a70ba06a95
Tushar Goyal <tgoyal63@gmail.com>
2024-05-12 18:06:05 +0530

    Drop slash command

---
+++ src/commands/slash/utility/drop.command.ts
@@ -0,0 +1,90 @@
+import type { SlashCommand } from "@/types/commands";
+import {
+  BaseGuildTextChannel,
+  Message,
+  SlashCommandBuilder,
+  type CommandInteraction,
+} from "discord.js";
+import ms from "ms";
+
+export const drop: SlashCommand = {
+  data: new SlashCommandBuilder()
+    .setName("drop")
+    .setDescription(
+      "Creates a drop in the selected channel with the specified message!"
+    )
+    .addChannelOption((option) =>
+      option
+        .setName("channel")
+        .setDescription("The channel in which you want to drop")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("message")
+        .setDescription("The message you want to show for drop")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("word")
+        .setDescription("The claim word which user will enter")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("duration")
+        .setDescription("Duration of the drop, defaults to 1h.")
+    )
+    .addStringOption((option) =>
+      option
+        .setName("victory-message")
+        .setDescription("The victory message, winning user will receive")
+    )
+    .setDefaultMemberPermissions(0)
+    .setDMPermission(false),
+  async execute(interaction: CommandInteraction) {
+    // Fetch the guild of the interaction
+    const guild = interaction.guild;
+
+    // Fetch the guild id
+    if (!guild) {
+      await interaction.reply("This command can only be used in a guild.");
+      return;
+    }
+
+    // Fetch the data
+    const channelId = interaction.options.get("channel", true).value as string;
+    const message = interaction.options.get("message", true).value as string;
+    const word = interaction.options.get("word", true).value as string;
+    const duration = interaction.options.get("duration")?.value as string;
+    const victoryMessage = interaction.options.get("victory-message")?.value as string || "You have won the drop.";
+
+    const durationInMs = duration ? ms(duration) : ms("1h");
+    const channel = guild.channels.cache.get(channelId) as BaseGuildTextChannel;
+
+    // Drop the message in the channel
+    const drop = await channel.send(message);
+    const link = `https://discord.com/channels/${guild.id}/${channelId}/${drop.id}`;
+
+    // Send the reply
+    interaction.reply({
+      content: "Drop created successfully\n" + link,
+      ephemeral: true,
+    });
+
+    // Message collector
+    const collectorFilter = (m: Message) => m.content === word;
+    try {
+      const claimed = await channel.awaitMessages({
+        filter: collectorFilter,
+        max: 1,
+        time: durationInMs,
+        errors: ["time"],
+      });
+      claimed.first()?.reply(victoryMessage);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
--- src/commands/slash/utility/index.ts
+++ src/commands/slash/utility/index.ts
@@ -1,3 +1,4 @@
 export * from "./ping.command";
 export * from "./modmailConfig.command";
+export * from "./drop.command";
 // export * from "./wv.command";
