9d0a6c2021e26d422e4f598cc4a9028053a0226a
Tushar Goyal <tgoyal63@gmail.com>
2024-06-24 14:31:01 +0530

    DB Connetion Util enhance

--- src/utils/dbConn.utils.ts
+++ src/utils/dbConn.utils.ts
@@ -1,9 +1,33 @@
 import mongoose from "mongoose";
 import { mongoUri } from "../config/config";
 
-export default async function dbConnect() {
-  if (mongoose.connection.readyState >= 1) {
-    return;
-  }
+/**
+ * Connects to MongoDB
+ * @returns {Promise<void>}
+ * @throws {Error}
+ * @description Connects to MongoDB using the MONGO_URI config variable.
+ */
+export default async function dbConnect(): Promise<void> {
+  try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
+
+    console.log("Connecting to MongoDB...");
+    mongoose.set("strictQuery", false);
     await mongoose.connect(mongoUri);
+    console.log("Connected to MongoDB!");
+
+    mongoose.connection.on("error", (error: Error) => {
+      console.error("Connection error:", error);
+      throw error;
+    });
+    mongoose.connection.on("disconnected", () => {
+      console.warn("MongoDB disconnected, attempting reconnection...");
+      setTimeout(dbConnect, 5000);
+    });
+  } catch (error) {
+    console.error("Connection error:", error);
+    throw error;
+  }
 }
