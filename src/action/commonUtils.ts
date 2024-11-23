bec6549a5c73443124af2129ef6bc100d725cde8
Sidhant <starindustries05@gmail.com>
2024-11-23 07:04:17 +0530

    refactored to functions instead of arrow functions

--- src/action/commonUtils.ts
+++ src/action/commonUtils.ts
@@ -12,8 +12,7 @@ import {
   ButtonStyle,
 } from "discord.js";
 
-export const getUser = async (user: string | User | GuildMember) => {
-  // if user is type of user return user
+export async function getUser(user: string | User | GuildMember) {
   if (user instanceof User) return user;
   if (user instanceof GuildMember) return user.user;
   try {
@@ -22,51 +21,49 @@ export const getUser = async (user: string | User | GuildMember) => {
     console.error(`Failed to fetch user: ${error}`);
     throw new CustomDiscordError(`Failed to fetch user: ${error}`);
   }
-};
+}
 
 export function messageStickerAndAttachmentParser(message: Message) {
-  // check partial message , fetch , deal with emotes , add an option to remove mentions etc
   if (message.stickers.first()) {
     return {
       content: `${message.content}\nsticker: ${message.stickers.first()?.url}`,
     };
-  } else
+  } else {
     return { content: message.content, files: message.attachments?.toJSON() };
+  }
 }
 
 export function messageComponentParser(messageComponent: MessageComponent, disableButton = false) {
-
-  if(!messageComponent.buttons || messageComponent.buttons.length==0)
-    return{
+  if (!messageComponent.buttons || messageComponent.buttons.length == 0) {
+    return {
       content: messageComponent.message.content,
       embeds: messageComponent.message.embeds,
-      files:[], // attachments to be implemented from messageComponent.message.attachments
+      files: [], // attachments to be implemented from messageComponent.message.attachments
       components: [],
-    }; 
-  
-  const parsedButtons = messageComponent?.buttons?.map(btn=>{
+    };
+  }
+
+  const parsedButtons = messageComponent?.buttons?.map((btn) => {
     return new ButtonBuilder()
-    .setCustomId(btn.label)
-    .setLabel(btn.label)
-    .setDisabled(disableButton)
-    .setStyle(btn.style || ButtonStyle.Primary);
-  })
+      .setCustomId(btn.label)
+      .setLabel(btn.label)
+      .setDisabled(disableButton)
+      .setStyle(btn.style || ButtonStyle.Primary);
+  });
 
   const row = new ActionRowBuilder<ButtonBuilder>().addComponents(parsedButtons);
 
   return {
     content: messageComponent.message.content,
     embeds: messageComponent.message.embeds,
-    files:[], // attachments to be implemented from messageComponent.message.attachments
-    components: [row]
-  }; 
+    files: [], // attachments to be implemented from messageComponent.message.attachments
+    components: [row],
+  };
 }
 
-export const getGuild = (guild: string | Guild) => {
-  // if guild is type of Guild return guild
+export function getGuild(guild: string | Guild) {
   if (guild instanceof Guild) return guild;
 
-  // if guild is type of string return guild
   try {
     const fetchedGuild = client.guilds.cache.get(guild);
     if (!fetchedGuild) {
@@ -77,19 +74,18 @@ export const getGuild = (guild: string | Guild) => {
     console.error(`Failed to fetch guild: ${error}`);
     throw new CustomDiscordError(`Failed to fetch guild: ${error}`);
   }
-};
+}
 
-export const getBan = async ({ guild, user }: { guild: Guild; user: User }) => {
+export async function getBan({ guild, user }: { guild: Guild; user: User }) {
   try {
-    // Fetch the ban
     return await guild.bans.fetch({ user, force: true });
   } catch (error) {
     console.error(`Failed to fetch ban: ${error}`);
   }
   return null;
-};
+}
 
-export const getRole = (role: string | Role, guild: string | Guild) => {
+export function getRole(role: string | Role, guild: string | Guild) {
   const fetchedGuild = getGuild(guild);
   if (role instanceof Role) return role;
   try {
@@ -102,12 +98,12 @@ export const getRole = (role: string | Role, guild: string | Guild) => {
     console.error(`Failed to fetch role: ${error}`);
     throw new CustomDiscordError(`Failed to fetch role: ${error}`);
   }
-};
+}
 
-export const getMember = async (
+export async function getMember(
   member: string | User | GuildMember,
   guild: string | Guild
-) => {
+) {
   if (member instanceof GuildMember) return member;
   const fetchedGuild = getGuild(guild);
   try {
@@ -117,4 +113,4 @@ export const getMember = async (
     console.error(`Failed to fetch member: ${error}`);
     throw new CustomDiscordError(`Member not found in the guild.`);
   }
-};
+}
